13fa3f597f966c0a1990a3053036c4b76b21ddb2
intermittent-reinforcement <omchizh@gmail.com>
2023-08-18 22:50:10 -0500

    3 increment

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
+	//"github.com/go-chi/chi"
 	"github.com/intermittent-reinforcement/shortener-proj/internal/app"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Get("/{id}", app.GetOrigPageRedir)
-	r.Post("/", app.PostShortURL)
+	// r := chi.NewRouter()
+	// r.Get("/{id}", app.GetOrigPageRedir)
+	// r.Post("/", app.PostShortURL)
 
-	err := http.ListenAndServe(`:8080`, r)
+	err := http.ListenAndServe(`:8080`, app.RootRouter())
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,15 +3,13 @@ module github.com/intermittent-reinforcement/shortener-proj
 go 1.20
 
 require (
-	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/chi v1.5.4
 	github.com/itchyny/base58-go v0.2.1
 	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-resty/resty/v2 v2.7.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
